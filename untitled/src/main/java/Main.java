d41c5b4270e3a0337c52cc7915cc12e345749500
git-sergBond <git-serg.bond@yandex.ru>
2023-04-25 23:22:12 +0300

    first

--- untitled/src/main/java/Main.java
+++ untitled/src/main/java/Main.java
@@ -1,5 +1,45 @@
-public class Main {
-    public static void main(String[] args) {
+import java.io.IOException;
+import java.net.*;
+import java.nio.charset.StandardCharsets;
 
+public class Main {
+    /**
+     * Программа создает сокет,
+     * соединяется с сервером (порт 6789), пересылает
+     * ему сообщение и ждет ответа.
+     * @param args
+     */
+    public static void main(String[] args) {
+        try {
+            DatagramSocket socket = new DatagramSocket(6789);
+
+            sendMessage(socket, "hello", "localhost", 6789);
+
+            receiveMessage(socket);
+
+            socket.close();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static void sendMessage(DatagramSocket socket,
+                                    String message,
+                                    String ip, int port) throws IOException {
+        byte[] byteMessage = message.getBytes(StandardCharsets.UTF_8);
+        InetAddress host = InetAddress.getByName(ip);
+
+        DatagramPacket request = new DatagramPacket(byteMessage, byteMessage.length, host, port);
+
+        socket.send(request);
+    }
+
+    private static void receiveMessage(DatagramSocket socket) throws IOException {
+        byte[] buffer = new byte[1000];
+        DatagramPacket response = new DatagramPacket(buffer, buffer.length);
+        socket.receive(response);
+
+        System.out.println("Response: " + new String(response.getData()));
     }
 }
