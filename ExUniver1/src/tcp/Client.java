d399f299d97b55e91c3fce7280421f4ee4201045
git-sergBond <git-serg.bond@yandex.ru>
2023-04-27 15:48:42 +0300

    finish TCP client-server application

--- ExUniver1/src/tcp/Client.java
+++ ExUniver1/src/tcp/Client.java
@@ -4,6 +4,7 @@ import java.io.DataInputStream;
 import java.io.DataOutputStream;
 import java.io.IOException;
 import java.net.Socket;
+import java.util.Scanner;
 
 public class Client {
 
@@ -12,15 +13,34 @@ public class Client {
     private static final String GET_ALL_CLIENTS = "GET /clients";
 
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        while (true) {
+            System.out.println("1) " + GET_ALL_CLIENTS);
+            System.out.println("2) " + GET_CLIENT_BY_ACCOUNT_ID);
+            System.out.println("Or type \"exit\" for escape program");
+
+            String answer = scanner.nextLine();
+            if (answer.startsWith("exit")) {
+                break;
+            } else if (answer.startsWith("1")) {
+                send(GET_ALL_CLIENTS);
+            } else if (answer.startsWith("2")) {
+                System.out.println("Enter account ID:");
+                String accountId = scanner.nextLine();
+
+                send(GET_CLIENT_BY_ACCOUNT_ID + accountId);
+            }
+        }
+    }
+
+    private static void send(String request) {
         int port = 7896;
         try (Socket socket = new Socket("localhost", port)) {
             DataInputStream inputStream = new DataInputStream(socket.getInputStream());
             DataOutputStream outputStream = new DataOutputStream(socket.getOutputStream());
-
-            outputStream.writeUTF(GET_ALL_CLIENTS);
-
-            String data = inputStream.readUTF();
-            System.out.println("Received: " + data);
+            outputStream.writeUTF(request);
+            String response = inputStream.readUTF();
+            System.out.println("Received: " + response);
         } catch (IOException e) {
             e.printStackTrace();
         }
